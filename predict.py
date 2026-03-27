58b1c2021b6ab16df7c64a611681e833df2c4cda
VO THANH LAM <vothanhlam@Mac-cua-Lam.local>
2026-03-27 07:09:06 +0700

    add sth

--- predict.py
+++ predict.py
@@ -6,32 +6,26 @@ import warnings
 warnings.filterwarnings("ignore")
 from ultralytics import YOLO
 
-print("⏳ Loading YOLO model...")
+print("Loading YOLO model...")
 # Đảm bảo bạn có file .pt từ Roboflow ở đây
 yolo_model = YOLO('license_plate_detector.pt')
 
-print("⏳ Loading EasyOCR...")
+print("Loading EasyOCR...")
 # Khởi tạo EasyOCR cho tiếng Anh
 reader = easyocr.Reader(['en'], gpu=False)
 
+#Em áp dụng hậu xử lý dựa trên domain knowledge để cải thiện độ chính xác OCR.
+def process_plate_text(raw_text): #sửa lại cho đúng format của biển số vn
 
-def process_plate_text(raw_text):
-    """
-    Hậu xử lý THUẦN TÚY dựa trên logic và định dạng biển số VN.
-    Cập nhật thêm kinh nghiệm nhận diện sai của AI (Q->0, 4->A).
-    """
     cleaned_text = re.sub(r'[^A-Z0-9]', '', raw_text.upper())
 
     if len(cleaned_text) < 4:
         return cleaned_text
 
-    char_list = list(cleaned_text)
+    char_list = list(cleaned_text) #chuyển thanh list
 
-    # --- CẬP NHẬT TỪ ĐIỂN KINH NGHIỆM ---
-    # Thêm 'Q': '0' (AI hay nhầm O, D, Q thành số 0)
     num_map = {'O': '0', 'D': '0', 'I': '1', 'L': '1', 'Z': '2', 'B': '8', 'S': '5', 'G': '6', 'Q': '0'}
 
-    # Thêm '4': 'A' (AI hay nhầm chữ A thành số 4)
     char_map = {'0': 'D', '1': 'I', '2': 'Z', '5': 'S', '8': 'B', '6': 'G', '3': 'B', '4': 'A'}
 
     # Luật 1: Ký tự 1 & 2 BẮT BUỘC là SỐ (Mã tỉnh)
@@ -58,7 +52,7 @@ def process_image(image_path):
 
     for r in results:
         boxes = r.boxes
-        for box in boxes:
+        for box in boxes:# xét từng bounding box tại vì 1 ảnh có thể có nhiều biển
             x1, y1, x2, y2 = map(int, box.xyxy[0])
             plate_crop = img[y1:y2, x1:x2]
             if plate_crop.size == 0: continue
@@ -66,9 +60,14 @@ def process_image(image_path):
             # --- Pre-processing cơ bản ---
             gray = cv2.cvtColor(plate_crop, cv2.COLOR_BGR2GRAY)
             large = cv2.resize(gray, None, fx=2.5, fy=2.5, interpolation=cv2.INTER_CUBIC)
+            # none là ko truyền kích thước cố định, fx là theo chiều ngang, fy ...
+            # interpolation=cv2.INTER_CUBIC : làm ảnh mượt, nét hơn, nội quy bậc 3
+            # bậc 1(nearest): copy pixel gần nhất. bậc 3(cubic) lấy 16pixel để tạo ra 1 pixel mới
 
             # --- ĐỌC OCR ---
             ocr_results = reader.readtext(large, detail=0, paragraph=True)
+            # detail = 0 là trả về text thôi, 1 là có trả về tọa độ nữa
+            #paragraph=True :  gom nhiều thành phần lại vơới nhau , cd 51f và 12344
 
             raw_text = "".join(ocr_results) if len(ocr_results) > 0 else "UNKNOWN"
             final_text = process_plate_text(raw_text)
@@ -91,7 +90,7 @@ def process_image(image_path):
 
 
 if __name__ == '__main__':
-    images = ["test_car.jpg", "test_car1.jpg", "test_car2.jpg", "test_car3.jpg", "test_car4.jpg", "test_car5.jpg",
+    images = ["test_car.jpg", "test_car1.jpg", "test_car3.jpg", "test_car4.jpg", "test_car5.jpg","test_moto1.jpg","test_moto2.jpg",
               "test_car6.jpg"]
     for img_name in images:
         try:
